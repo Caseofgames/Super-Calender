97cfa87b3dea86e0c54fc7cb8cac00a1709ed86a
caseofminecraft <Family@Family-PC.attlocal.net>
2014-10-20 17:01:07 -0500

    The Main File For The Program

--- src/com/caseofgames/supercalender/client/Main.java
+++ src/com/caseofgames/supercalender/client/Main.java
@@ -1,5 +1,12 @@
 package com.caseofgames.supercalender.client;
 
-public class Main {
+import javax.swing.JFrame;
 
+public class Main extends JFrame {
+	
+	private static final long serialVersionUID = 9038252099291970573L;
+	
+	public static void main(String[] args) {
+		
+	}
 }
